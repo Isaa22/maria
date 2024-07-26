cffb529b0b5ed1f36dacf5bc1337e2c5dec54063
Isaa22 <m.carvalho25@escola.pr.gov.br>
2024-07-26 20:50:33 -0300

    isinha

--- main.js
+++ main.js
@@ -28,13 +28,13 @@ function draw() {
     drops[i]++;
   }
 
-  // Adiciona o nome "Rafael" centralizado
+  // Adiciona o nome "isa" centralizado
   ctx.fillStyle = '#00FF00'; // Cor verde
   ctx.font = 'bold 24px monospace';
-  const textWidth = ctx.measureText('Rafael').width;
+  const textWidth = ctx.measureText('isa').width;
   const x = (canvas.width - textWidth) / 2;
   const y = canvas.height / 2;
-  ctx.fillText('Rafael Assis Santos', x, y);
+  ctx.fillText('Maria Isabely Soares Carvalho', x, y);
 
   requestAnimationFrame(draw);
 }
